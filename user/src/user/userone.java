a689232ee93bd1ea09126be90591958ab8705c52
suresh <suresh@suresh-VAIO>
2017-11-15 21:39:36 +0530

    first tym commiting code

---
+++ user/src/user/userone.java
@@ -0,0 +1,11 @@
+package user;
+
+public class userone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is userone project");
+
+	}
+
+}
